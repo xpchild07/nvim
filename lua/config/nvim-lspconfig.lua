8548fa16aaf6015129d2e5e011a3c3e8b211bd4b
xpchild07 <46098834+xpchild07@users.noreply.github.com>
2022-11-14 17:53:35 +0800

    Update nvim-lspconfig.lua

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -1,19 +1,70 @@
--- 诊断样式
-vim.diagnostic.config(
-    {
-        -- 诊断虚拟文本
-        virtual_text = {
-            -- 显示前缀
-            prefix = "●",
-            -- 是否总是显示前缀
-            source = "always"
+local lspconfig = require('lspconfig')
+lspconfig.ccls.setup {
+    single_file_support = true,
+    init_options = {
+        cache = {
+            directory = "~/.cache/ccls";
         },
-        float = {
-            -- 是否显示诊断来源
-            source = "always"
+        highlight = {
+            lsRanges = true;
         },
-
-        -- 在插入模式下不显示诊断信息
-        update_in_insert = false
+    },
+    filetypes = {
+        "c", "cpp", "cc"
     }
+}
+
+vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, bufopts)
+vim.keymap.set('n', '<C-M>', vim.lsp.buf.hover, bufopts)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+vim.keymap.set('n', '<space>r', vim.lsp.buf.references, bufopts)
+vim.keymap.set('n', '<space>n', vim.lsp.buf.rename, bufopts)
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+  }
 )
+
+vim.g.python2_host_prog = '/usr/bin/python'
+
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+        expand = function(args)
+             vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = {
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'nvim_lsp' },
+    }
+})
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
